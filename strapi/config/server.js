9ae738fa668786ad87ef716e6b5bbbc63aae7b8d
Alex <alexmcgil@vivaldi.net>
2022-01-24 01:49:29 +0300

    Initial commit

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0985b26aec1da7c7f25162717c0bd2a'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e54799f-7a9e-4617-8cd9-0f3ff6a9765c'
+};
