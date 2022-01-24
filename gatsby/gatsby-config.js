231c90455a4c30cef885924f7905861d16fdeae2
Alex <alexmcgil@vivaldi.net>
2022-01-24 06:16:20 +0300

    Finished receiving data from strapi using gatsby-source-strapi. Temporarily disabled npm update.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -4,6 +4,17 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: `gatsby-source-strapi`,
+            options: {
+                apiURL: `http://localhost:1337`,
+                contentTypes: [
+                    `article`,
+                    `user`,
+                ],
+                queryLimit: 1000,
+                collectionTypes: [`article`, `user`],
+            }
+        }
     ]
 }
